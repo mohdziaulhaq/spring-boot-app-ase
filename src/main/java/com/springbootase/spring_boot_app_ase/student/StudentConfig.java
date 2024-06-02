17880652133f55cfd8f490c377d05082c67c3a5d
Mohd Zia Ul Haq <mohdziaulhaq123@gmail.com>
2024-06-02 17:15:11 -0500

    created student config

--- src/main/java/com/springbootase/spring_boot_app_ase/student/StudentConfig.java
+++ src/main/java/com/springbootase/spring_boot_app_ase/student/StudentConfig.java
@@ -1,2 +1,31 @@
-package com.springbootase.spring_boot_app_ase.student;public class StudentConfig {
+package com.springbootase.spring_boot_app_ase.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(
+            StudentRepository repository){
+        return args -> {
+            Student zia =  new Student(1L,
+                            "Zia",
+                            "zia@gmail.com",
+                            LocalDate.of(1999, Month.DECEMBER, 5));
+
+            Student yutong =  new Student(1L,
+                    "Yutong",
+                    "yutong@gmail.com",
+                    LocalDate.of(1999, Month.DECEMBER, 5));
+
+            repository.saveAll(List.of(zia,yutong));
+        };
+    }
 }
